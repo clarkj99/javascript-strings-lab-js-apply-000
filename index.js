249e95c7d1a4b1062565f3bad2f41827da0a64a9
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-08-28 19:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "hello, Neil deGrasse Tyson!";
+var specialGuest = "Hello, Neil deGrasse Tyson!";
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
